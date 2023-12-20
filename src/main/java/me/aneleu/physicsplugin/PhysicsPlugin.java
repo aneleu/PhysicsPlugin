53b153e86e22e0674a9ff34da79af2d427643a1b
aneleu <aneleu1324@gmail.com>
2023-12-20 21:00:24 +0900

    test

---
+++ src/main/java/me/aneleu/physicsplugin/PhysicsPlugin.java
@@ -0,0 +1,13 @@
+package me.aneleu.physicsplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PhysicsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        getLogger().info("PhysicsPlugin enabled!");
+
+    }
+}
